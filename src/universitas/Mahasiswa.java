36810d7ac714e198fe7cec788242b7e3b1bc95bb
wawachaan <najwanorizat4@gmail.com>
2023-06-13 08:56:09 +0700

    Membuat class Mahasiswa dan Mahasiswa beraksi

---
+++ src/universitas/Mahasiswa.java
@@ -0,0 +1,27 @@
+
+package universitas;
+
+
+public class Mahasiswa {
+      String nama;
+    String npm;
+    String prodi;
+    
+    public String getNama(){
+       return nama;  
+   }
+    public String getNpm(){
+       return npm;  
+   }
+    public String getProdi(){
+       return prodi;  
+   }
+    
+    public void setnama (String nama){
+       nama = nama;
+   }
+    
+    public void setnpm (String npm){
+       npm = npm;
+   }
+}
---
+++ src/universitas/MatematikanBeraksi.java
@@ -0,0 +1,7 @@
+
+package universitas;
+
+
+public class MatematikanBeraksi {
+    
+}
